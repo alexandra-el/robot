1875a056ae7268e48039a54cc2f1f60254aacd64
alexandra-el <120280363+alexandra-el@users.noreply.github.com>
2022-12-17 13:12:38 +0700

    Update animation.py

--- animation.py
+++ animation.py
@@ -1,3 +1,4 @@
+import numpy
 import matplotlib.pyplot as pyplot
 
 class Animation:
@@ -89,4 +90,3 @@ class Animation:
         self.canvas.set_data(self.image)
         pyplot.draw()
         pyplot.pause(0.0001)
-
